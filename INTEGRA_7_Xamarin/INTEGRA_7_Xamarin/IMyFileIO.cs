72933bcadd69dc45e0942752005f2245c3a76fbe
the51craw <35884608+the51craw@users.noreply.github.com>
2018-11-14 09:56:20 +0100

    Renamed files and namespaces

---
+++ INTEGRA_7_Xamarin/INTEGRA_7_Xamarin/IMyFileIO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integra_7_Xamarin
+{
+    public interface IMyFileIO
+    {
+        void GenericHandler(object sender, object e);
+
+        void SetMainPagePortable(Integra_7_Xamarin.MainPage mainPage);
+
+        String ReadFile(String filename);
+
+        void LoadFavorites();
+    }
+}
---
+++ INTEGRA_7_Xamarin/INTEGRA_7_Xamarin/MotionalSurround.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integra_7_Xamarin
+{
+    public partial class UIHandler
+    {
+    }
+}
